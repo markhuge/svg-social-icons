86a14adf03cbaf4f63f21f0977b08da019776ddc
markhuge <mark@segfawlt.net>
2015-08-22 01:09:46 -0700

    color override

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var icons = require('./lib/icons.json');
 
 module.exports = function (network, opts) {
+  var data = icons[network];
+  
   var opts            = opts || {},
       svgClass        = opts.svgClass        || 'social-svg',
       backgroundClass = opts.backgroundClass || 'social-svg-background',
       iconClass       = opts.iconClass       || 'social-svg-icon',
-      maskClass       = opts.maskClass       || 'social-svg-mask';
+      maskClass       = opts.maskClass       || 'social-svg-mask',
+      color           = opts.color           || data.color;
   
-  var data = icons[network];
-  return '<svg class="' + svgClass + '" viewBox="0 0 64 64" ><g class="' + backgroundClass + '" ><circle cx="32" cy="32" r="31" ></circle></g><g class="' + iconClass + '" ><path d="' + data.icon + '" ></path></g><g class="' + maskClass + '" style="fill:' + data.color + '" ><path d="' + data.mask +'" ></path></g></svg>';
+  return '<svg class="' + svgClass + '" viewBox="0 0 64 64" ><g class="' + backgroundClass + '" ><circle cx="32" cy="32" r="31" ></circle></g><g class="' + iconClass + '" ><path d="' + data.icon + '" ></path></g><g class="' + maskClass + '" style="fill:' + color + '" ><path d="' + data.mask +'" ></path></g></svg>';
 };
